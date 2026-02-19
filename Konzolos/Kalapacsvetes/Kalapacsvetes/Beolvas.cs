b77842dccf3f0a6434ba8e2fc7bddf7c0cbe50ae
GellertK15 <kiraly.gellert@gmail.com>
2026-02-19 11:05:19 +0100

    Konzolos projekt hozzáadása

---
+++ Konzolos/Kalapacsvetes/Kalapacsvetes/Beolvas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalapacsvetes
+{
+    internal class Beolvas
+    {
+        public static List<Sportolo> FileBeolvasas(string filepath)
+        {
+            List<Sportolo> sportolok = new List<Sportolo>();
+
+            using (StreamReader sr = new StreamReader("kalapacsvetes.txt"))
+            {
+                string line = sr.ReadLine();
+                if (line != null)
+                {
+                    Console.WriteLine(line);
+                }
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] adatok = line.Split(';');
+                    int helyezes = int.Parse(adatok[0]);
+                    double eredmeny = double.Parse(adatok[1]);
+                    string sportolonev = adatok[2];
+                    string orszagkod = adatok[3];
+                    string helyszin = adatok[4];
+                    DateTime datum = DateTime.Parse(adatok[5]);
+                    Sportolo sportolo = new Sportolo(helyezes, eredmeny, sportolonev, orszagkod, helyszin, datum);
+                    sportolok.Add(sportolo);
+                }
+            }
+            return sportolok;
+        }
+    }
+}
---
+++ Konzolos/Kalapacsvetes/Kalapacsvetes/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalapacsvetes
+{
+    internal class Program
+    {
+        static List<Sportolo> sportolok = new List<Sportolo>();
+        static void Main(string[] args)
+        {
+            sportolok = Beolvas.FileBeolvasas("kalapacsvetes.txt");
+            Feladat4();
+            Feladat5();
+            Feladat6();
+            Feladat7();
+            Feladat8();
+        }
+
+        public static void Feladat4()
+        {
+            // Határozza meg és írja ki, hány dobás eredménye található a forrásfájlban! 
+            Console.WriteLine($"4. feladat: {sportolok.Count} dobás eredménye található.");
+        }
+
+        public static void Feladat5()
+        {
+            //Határozza meg és jelenítse meg a forrásállományban szereplő magyar (HUN) sportolók dobásainak átlageredményét! Az eredményt két tizedesre kerekítve írja ki!
+            var magyarSportolok = sportolok.Where(s => s.Orszagkod == "HUN");
+            double atlagEredmeny = magyarSportolok.Average(s => s.Eredmeny);
+            Console.WriteLine($"5. feladat: A magyar sportolók dobásainak átlageredménye: {atlagEredmeny} métert dobtak.");
+        }
+
+        public static void Feladat6()
+        {
+            //Kérjen be egy évszámot és írja ki, hogy abban az évben mennyi dobás került be a legjobbak közé, illetve írja ki, hogy mely sportolók érték el ezeket.Ellenkező esetben írja ki, hogy az adott évben nem került be egy dobás eredménye sem a legjobbak közé
+            Console.Write("6. feladat: Kérem adjon meg egy évszámot: ");
+            int evszam = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Az adott évben {sportolok.Count(s => s.Datum.Year == evszam)} dobás került be a legjobbak közé.");
+            var sportolokEvben = sportolok.Where(s => s.Datum.Year == evszam);
+            Console.WriteLine($"Az adott évben a következő sportolók érték el ezeket: {string.Join(", ", sportolokEvben.Select(s => s.SportoloNev))}");
+
+        }
+
+        public static void Feladat7()
+        {
+            //Készítsen statisztikát, hogy melyik országból hány kalapácsvetés eredménye szerepel a legjobb dobások között. Az eredményt a mintának megfelelően írassa ki a képernyőre!
+            Console.WriteLine("7. feladat: Statisztika a legjobb dobások között szereplő országokról:");
+            var statisztika = sportolok.GroupBy(s => s.Orszagkod)
+                .Select(g => new { Orszagkod = g.Key, DobasokSzama = g.Count() })
+                .OrderByDescending(s => s.DobasokSzama);
+            Console.WriteLine("Országkód\tDobások száma");
+            foreach (var item in statisztika)
+            {
+                Console.WriteLine($"{item.Orszagkod}\t\t{item.DobasokSzama}");
+            }
+        }
+
+        public static void Feladat8()
+        {
+            //Hozzon létre magyarok.txt néven egy UTF-8 kódolású fájlt, amelyben csak a magyar (HUN)sportolók eredményei szerepelnek.
+            var magyarSportolok = sportolok.Where(s => s.Orszagkod == "HUN");
+            using (StreamWriter sw = new StreamWriter("magyarok.txt", false, Encoding.UTF8))
+            {
+                sw.WriteLine("Helyezés;Eredmény;Sportoló neve;Országkód;Helyszín;Dátum");
+                foreach (var sportolo in magyarSportolok)
+                {
+                    sw.WriteLine($"{sportolo.Helyezes};{sportolo.Eredmeny};{sportolo.SportoloNev};{sportolo.Orszagkod};{sportolo.Helyszin};{sportolo.Datum:yyyy-MM-dd}");
+                }
+            }
+        }
+    }
+
+
+}
